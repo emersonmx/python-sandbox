72c9cdc4b9040cc63ca5ee314ee4384ecbe9e60e
Emerson MX <emersonmx@gmail.com>
2019-07-14 15:34:17 -0300

    Add config file auto creation

--- code/tmplt/tmplt/__init__.py
+++ code/tmplt/tmplt/__init__.py
@@ -1,12 +1,29 @@
 import os
 import click
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-
 
 @click.group()
 def cli():
-    pass
+    _setup_config_file()
+    _setup_templates_directory()
+
+
+def _setup_config_file():
+    if os.path.isfile(CONFIG_FILEPATH):
+        return
+
+    os.makedirs(CONFIG_PATH, exist_ok=True)
+    with open(CONFIG_FILEPATH, 'w+') as cf:
+        configs = config.get_merged_configs()
+        cf.write(dumps_json(configs))
+        click.echo('Config file created in {}'.format(CONFIG_FILEPATH))
+
+
+def _setup_templates_directory():
+    template_path = get_config('template_path')
+    if not os.path.isdir(template_path):
+        os.makedirs(template_path, exist_ok=True)
 
 
 from tmplt import make, config
+from tmplt.config import CONFIG_PATH, CONFIG_FILEPATH, get_config, dumps_json
--- code/tmplt/tmplt/config.py
+++ code/tmplt/tmplt/config.py
@@ -2,11 +2,24 @@ import os
 import json
 import click
 from deepmerge import always_merger
-from tmplt import cli, PROJECT_PATH
+from tmplt import cli
 
 CONFIG_PATH = click.get_app_dir('tmplt')
 CONFIG_FILENAME = 'tmplt.json'
-CONFIG_FILE = os.path.join(CONFIG_PATH, CONFIG_FILENAME)
+CONFIG_FILEPATH = os.path.join(CONFIG_PATH, CONFIG_FILENAME)
+
+
+@cli.group()
+def config():
+    pass
+
+
+@config.command()
+def dump():
+    if not os.path.isfile(CONFIG_FILEPATH):
+        click.echo(dumps_json(get_default_configs()))
+        return
+    click.echo(dumps_json(get_merged_configs()))
 
 
 def dumps_json(obj):
@@ -15,7 +28,7 @@ def dumps_json(obj):
 
 def get_default_configs():
     return {
-        'template_path': os.path.join(PROJECT_PATH, 'templates')
+        'template_path': os.path.join(CONFIG_PATH, 'templates')
     }
 
 
@@ -37,16 +50,3 @@ def get_merged_configs():
 def get_config(config):
     configs = get_merged_configs()
     return configs.get(config)
-
-
-@cli.group()
-def config():
-    pass
-
-
-@config.command()
-def dump():
-    if not os.path.isfile(CONFIG_FILE):
-        click.echo(dumps_json(get_default_configs()))
-        return
-    click.echo(dumps_json(get_merged_configs()))
