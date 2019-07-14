7a9924fdd02abcd3c84cf9d7f1fb9cfc9bf34144
Emerson MX <emersonmx@gmail.com>
2019-07-14 19:10:45 -0300

    Update dependencies

--- code/tmplt/setup.py
+++ code/tmplt/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click'
+        'Click', 'Jinja2', 'deepmerge'
     ],
     entry_points='''
         [console_scripts]
