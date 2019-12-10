826af2ec118040c492bd53d616f4d3446f7ba536
Emerson MX <emersonmx@gmail.com>
2019-12-10 19:31:12 -0300

    Add blog

--- code/flaskr/app/__init__.py
+++ code/flaskr/app/__init__.py
@@ -7,14 +7,13 @@ def create_app(test_config=None):
     from app import config
     config.init_app(app)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
     from app import db
     db.init_app(app)
 
     from app import auth
     app.register_blueprint(auth.bp)
+    from app import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
