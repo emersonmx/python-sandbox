7315b1fcb394b48ccd87f988509dd413f410a0b1
Emerson MX <emersonmx@gmail.com>
2020-01-03 14:29:13 -0300

    Add SoftDelete mixin

--- code/taskr/app/mixins.py
+++ code/taskr/app/mixins.py
@@ -6,3 +6,7 @@ import sqlalchemy as sa
 class Timestamp(object):
     created_at = sa.Column(sa.DateTime, default=datetime.utcnow)
     updated_at = sa.Column(sa.DateTime, onupdate=datetime.utcnow)
+
+
+class SoftDelete(object):
+    deleted_at = sa.Column(sa.DateTime)
