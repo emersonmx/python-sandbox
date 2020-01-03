389ef174f0615e68be89bf1c2e700e592fad9f9b
Emerson MX <emersonmx@gmail.com>
2020-01-03 09:59:46 -0300

    Start taskr project

---
+++ code/taskr/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    from app import config
+    config.init_app(app)
+
+    from app import db
+    db.init_app(app)
+
+    return app
